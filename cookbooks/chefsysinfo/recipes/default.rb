5b622186ed9f18a9c4e0dc0da5bb1f7f0ac50716
opsadmin <opsadmin@fiyaslocal.com>
2020-02-23 15:33:33 +0000

    added the recipie

--- cookbooks/chefsysinfo/recipes/default.rb
+++ cookbooks/chefsysinfo/recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+#
+#
+file '/etc/systeminfo' do
+	content 'test file'
+
+	#Verify the file is already present 
+  	verify do |path|
+    		open(path).read.include? "test"
+  	end
+        mode '0600'
+#        chattr '+i'
+        owner 'root'
+        group 'root'
+end
